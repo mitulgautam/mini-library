3a1627b2b54c657bfd82b121a1220f1a86eaf4e2
img <mitulgautam@outlook.com>
2025-04-29 11:39:15 +0530

    Removed Validaitons

--- src/main/java/dev/mitulgautam/minilibrary/models/Book.java
+++ src/main/java/dev/mitulgautam/minilibrary/models/Book.java
@@ -1,38 +1,11 @@
 package dev.mitulgautam.minilibrary.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import jakarta.persistence.PrePersist;
-
 import java.util.UUID;
 
-@Entity
-@Getter
 public class Book {
-    @Id
-    @NotNull
-    @Column(nullable = false)
     UUID bookId;
-
-    @Column(nullable = false)
-    @Size(min = 5, max = 255, message = "Book title should be between 5 and 255 characters.")
     String title;
-
-    @Column(nullable = false)
-    @Size(min = 5, max = 255, message = "Book author should be between 5 and 255 characters.")
     String author;
-
-    @Column(nullable = false)
     boolean available;
 
-    @PrePersist
-    public void prePersist() {
-        if (bookId == null) {
-            bookId = UUID.randomUUID();
-        }
-    }
 }
--- src/main/java/dev/mitulgautam/minilibrary/models/Borrower.java
+++ src/main/java/dev/mitulgautam/minilibrary/models/Borrower.java
@@ -1,34 +1,12 @@
 package dev.mitulgautam.minilibrary.models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.PrePersist;
-import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-
 import java.util.UUID;
 
-@Entity
-@Getter
 public class Borrower {
-    @Id
-    @NotNull
-    @Column(nullable = false)
     UUID borrowerId;
 
-    @NotNull
-    @Column(nullable = false)
     String name;
 
-    @NotNull
-    @Column(nullable = false)
     String email;
 
-    @PrePersist
-    public void prePersist() {
-        if (borrowerId == null) {
-            borrowerId = UUID.randomUUID();
-        }
-    }
 }
