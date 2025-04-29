d1883a3863f1a656e3e10adbf56132496ea30862
img <mitulgautam@outlook.com>
2025-04-29 10:43:16 +0530

    Initialized mini library project.

---
+++ src/main/java/dev/mitulgautam/minilibrary/MiniLibraryApplication.java
@@ -0,0 +1,13 @@
+package dev.mitulgautam.minilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mitulgautam/minilibrary/MiniLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mitulgautam.minilibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
